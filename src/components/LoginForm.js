4b434394d6c5306fbc47f502f808fcb860b8ba1d
Cameron <camcreel@gmail.com>
2021-09-11 19:01:58 -0400

    Add _redirects file for front end deployment

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,8 @@
 function LoginForm() {
     return (
-        <div>
+        <form>
 
-        </div>
+        </form>
     )
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,6 +6,11 @@ function Profile({user}) {
             <div>location: {user.location}</div>
             <div>email: {user.email}</div>
             <div>job: {user.job_title}</div>
+            <div>
+                <div>Jobs</div>
+                <div>Active Tasks</div>
+                <div>Contacts</div>
+            </div>
         </div>
     )
 
