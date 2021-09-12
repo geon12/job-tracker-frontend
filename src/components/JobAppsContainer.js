9635443d3d089eaabb7545678c1f7aed5c778254
Cameron <camcreel@gmail.com>
2021-09-12 18:53:34 -0400

    change where job apps state is set

--- src/components/JobAppsContainer.js
+++ src/components/JobAppsContainer.js
@@ -1,16 +1,16 @@
 import { useEffect,useState } from "react"
 import JobAppCard from "./JobAppCard"
 
-function JobAppsContainer() {
-    const [jobApps, setJobApps] = useState(null)
-    useEffect( () => {
-        fetch(`${process.env.REACT_APP_API_URL}/job_applications`,{credentials:'include'})
-        .then((resp) => {
-            if (resp.ok) {
-            resp.json().then(setJobApps)
-            }
-        })
-    },[])
+function JobAppsContainer({jobApps}) {
+    // const [jobApps, setJobApps] = useState(null)
+    // useEffect( () => {
+    //     fetch(`${process.env.REACT_APP_API_URL}/job_applications`,{credentials:'include'})
+    //     .then((resp) => {
+    //         if (resp.ok) {
+    //         resp.json().then(setJobApps)
+    //         }
+    //     })
+    // },[])
     return (
         <div>
             {jobApps ? jobApps.map((app) => <JobAppCard app={app}/>) : <div>Page is Loading</div>}
