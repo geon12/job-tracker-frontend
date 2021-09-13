4d36d1eaa6e9d45dbfdefdf138655e2f8b30c309
Cameron <camcreel@gmail.com>
2021-09-12 22:04:45 -0400

    change mapping in jobappscontainer

--- src/components/JobAppsContainer.js
+++ src/components/JobAppsContainer.js
@@ -2,7 +2,7 @@ import { useEffect,useState } from "react"
 import JobAppCard from "./JobAppCard"
 
 function JobAppsContainer() {
-    const [jobApps, setJobApps] = useState(null)
+    // const [jobApps, setJobApps] = useState(null)
     // const [tasks, setTasks] = useState(null)
     // const [contacts,setContacts] = useState(null)
     useEffect( () => {
@@ -18,7 +18,7 @@ function JobAppsContainer() {
     },[])
     return (
         <div>
-            {jobApps ? jobApps.map((app) => <JobAppCard app={app}/>) : <div>Page is Loading</div>}
+            {/* {jobApps ? jobApps.map((app) => <JobAppCard app={app}/>) : <div>Page is Loading</div>} */}
         </div>
     )
 }
