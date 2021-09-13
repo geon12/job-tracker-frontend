46928844690ac28277592e6baf1304aa88a4fca6
Cameron <camcreel@gmail.com>
2021-09-13 12:40:17 -0400

    add more detail to job app card

--- src/components/JobAppCard.js
+++ src/components/JobAppCard.js
@@ -2,10 +2,12 @@ function JobAppCard({app}) {
    
     return (
         <div>
-            <h1>{app.job.role}</h1>
-            {/* <h2>{app.job.organizaion.name}</h2> */}
-            <h2>{app.notes}</h2>
-            <h2>{app.application_process}</h2>
+            <h1>job: {app.job.role}</h1>
+            <h2>org: {app.job.organization.name}</h2>
+            <h2>notes: {app.notes}</h2>
+            <h2>application method-{app.application_process}</h2>
+            <h3>rejected-{app.rejected}</h3>
+            <h3>status-{app.status}</h3>
         </div>
     )
 }
