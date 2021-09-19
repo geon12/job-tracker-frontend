4d17ea40f9f8d088bc114c6d5ba23d71e47bd22e
Cameron <camcreel@gmail.com>
2021-09-19 16:46:22 -0400

    add image and style button on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,10 @@ function Home() {
 
     return (
         <div className="text-center">
-            <h1>Track Your Job Applications</h1>
-            
-            <Link to="/signup"><button className="text-center" >Try It Out</button></Link>
+            <h1 className="display-1">Track Your Job Applications</h1>
+            <img className="my-2 mb-4" src="interview-image.jpg" alt="interview over zoom"/>
+            <br />
+            <Link to="/signup"><button className="btn btn-secondary btn-lg">Try It Out !</button></Link>
         </div>
     )
 }
