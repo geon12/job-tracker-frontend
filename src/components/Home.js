b1587aefe91513940ba3669437dcd63ebb067129
Cameron <camcreel@gmail.com>
2021-09-19 15:16:37 -0400

    style Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import {Link} from "react-router-dom";
 function Home() {
 
     return (
-        <div>
+        <div className="text-center">
             <h1>Track Your Job Applications</h1>
-            <Link to="/signup"><button>Try It Out</button></Link>
+            
+            <Link to="/signup"><button className="text-center" >Try It Out</button></Link>
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,26 @@
 import {Link} from "react-router-dom";
 import LogOut from "./LogOut";
+import './styles/NavBar.css'
 
 function NavBar({user, setUser}) {
+
+    
     return (
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          {user ?  
-            <>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                    <Link to="/job_applications">Job Applications</Link>
-                </li>
-                <li>
-                    <LogOut setUser={setUser}/>
-                </li>
-            </> :
-            <li>
-                <Link to="/login"> 
-                    <button>Login</button>
-                </Link>
-            </li>
-            }
-        </ul>
+        <nav className="navbar navbar-expand-lg">
+                    <Link className="nav-link nav-item navbar-brand" to="/">JobTracker</Link>
+                {user ?  
+                    <>
+                        
+                            <Link className="nav-link nav-item" to="/profile">Profile</Link>
+                            <Link className="nav-link nav-item" to="/job_applications">Job Applications</Link>
+                            <LogOut className="nav-item" setUser={setUser}/>
+    
+                    </> :
+                        <Link className="nav-link nav-item" to="/login"> 
+                            <button>Login</button>
+                        </Link>
+                    }
+        </nav>
     )
 }
 
